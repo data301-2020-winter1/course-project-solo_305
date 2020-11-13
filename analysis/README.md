77edfb76f885092e2bdd8c694ba92f2fd1b448d8
Jo-Elle Craig <jo-elle@Jo-Elles-MacBook-Air.local>
2020-11-12 17:58:25 -0800

    Milestone 2, tidied data, renamed columns, and prep work for EDA complete

--- analysis/README.md
+++ analysis/README.md
@@ -1,2 +1,4 @@
 ***In this direcotry I will be performing my analysis on my data set on credit card default rates***
+#I hope to answer my research question regarding whether there are correlations between credit card default and people 
+
 
---
+++ analysis/scripts/.ipynb_checkpoints/project_functions-checkpoint.py
@@ -0,0 +1,9 @@
+def load_process(path_to_csv_file):
+    df = (pd.read_csv(path_to_csv_file)
+          .drop(columns={'BILL_AMT1', 'BILL_AMT2', 'BILL_AMT3', 'BILL_AMT4', 'BILL_AMT5', 'BILL_AMT6'})
+          .drop(columns={"PAY_AMT1",'PAY_AMT2','PAY_AMT3','PAY_AMT4','PAY_AMT5','PAY_AMT6'})
+          .rename(columns={"PAY_0": "PAY_1"})
+          .rename(columns={"default.payment.next.month":'Default'})
+         )
+    return df
+load_process('/Users/jo-elle/Desktop/Data301/project/course-project-solo_305/data/raw.data/UCI_Credit_Card.csv')
